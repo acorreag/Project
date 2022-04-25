c6b6d4164b9481921bfd98c166f0525ef4f42b70
acorreag <acorreagarcia1@babson.edu>
2022-04-25 18:54:17 -0400

    mobile friendly

---
+++ app.js
@@ -0,0 +1,12 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.navlinks');
+
+//     burger.addEventListener('click', ()=>{
+//         nav.classList.toggle('nav-active');
+//     } );
+//     console.log("complete")
+// }
+
+// navSlide();
+
